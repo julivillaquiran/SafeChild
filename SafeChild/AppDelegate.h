ccbbc4afb1767a361abfbc437e5636cf241a0bc2
JulianV <juliscout00@hotmail.com>
2015-04-17 08:28:04 -0500

    Initial Commit

---
+++ SafeChild/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SafeChild
+//
+//  Created by Julian Villaquiran on 17/04/15.
+//  Copyright (c) 2015 Julian Villaquiran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SafeChild/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SafeChild
+//
+//  Created by Julian Villaquiran on 17/04/15.
+//  Copyright (c) 2015 Julian Villaquiran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
